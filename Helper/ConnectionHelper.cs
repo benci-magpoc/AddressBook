bae750b0b19f48bc1ced1c5b17eb45c5b2e9ff0b
Benci Magpoc <BENCIIAN@GMAIL.COM>
2022-04-29 12:38:13 -0400

    added images

---
+++ Helper/ConnectionHelper.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+
+namespace AddressBook.Helper
+{
+    public class ConnectionHelper
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+            return builder.ToString();
+        }
+    }
+}
